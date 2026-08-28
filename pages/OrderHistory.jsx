801fed0600e94daf8641b318ad5933fa9f1763bb
Abdul Wahab Nadeem <wahabndm@gmail.com>
2026-08-28 17:02:47 +0500

    fix/path

--- pages/OrderHistory.jsx
+++ pages/OrderHistory.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getMyOrders } from "../services/orderService";
-import { API_BASE_URL } from "../utils/constants";
+import { API_BASE_URL } from "../utils/constant";
 import Loader from "../components/common/Loader";
 
 export default function OrderHistory() {
